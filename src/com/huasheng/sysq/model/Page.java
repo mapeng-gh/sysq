9a8ebdb5f878a8eec3984ae62115db09e33a6223
mapeng <mapeng3@55tuan.com>
2016-03-01 21:38:35 +0800

    添加常量类、分页对象、通用样式

---
+++ src/com/huasheng/sysq/model/Page.java
@@ -0,0 +1,45 @@
+package com.huasheng.sysq.model;
+
+import java.util.List;
+
+public class Page<T> {
+
+	private List<T> data;
+	private int pageNo;
+	private int pageSize;
+	private int totalPages;
+	
+	public static int PAGE_SIZE = 20;
+
+	public List<T> getData() {
+		return data;
+	}
+
+	public void setData(List<T> data) {
+		this.data = data;
+	}
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getTotalPages() {
+		return totalPages;
+	}
+
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+}
---
+++ src/com/huasheng/sysq/util/Constants.java
@@ -0,0 +1,26 @@
+package com.huasheng.sysq.util;
+
+public class Constants {
+
+	/**
+	 * 所有表名
+	 */
+	public static final String TABLE_INTERVIEWER = "interviewer";
+	public static final String TABLE_RESERVATION = "reservation";
+	
+	/**
+	 * 预约表所有列名
+	 */
+	public static final String TABLE_RESERVATION_COLUMN_NAME = "name";
+	public static final String TABLE_RESERVATION_COLUMN_IDENTITY_CARD = "identity_card";
+	public static final String TABLE_RESERVATION_COLUMN_MOBILE = "mobile";
+	public static final String TABLE_RESERVATION_COLUMN_INTERVIEW_TYPE = "interview_type";
+	public static final String TABLE_RESERVATION_COLUMN_BOOK_DATE = "book_date";
+	public static final String TABLE_RESERVATION_COLUMN_FAMILY_MOBILE = "family_mobile";
+	public static final String TABLE_RESERVATION_COLUMN_STATUS = "status";
+	
+	/**
+	 * 日期格式
+	 */
+	public static final String PATTERN_YEAR_MONTH_DAY_HOUR_MINUTE = "yyyy-MM-dd HH:mm";
+}
