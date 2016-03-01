77c541060b8c7747b99196996a10b09ef6d2fa4e
mapeng <mapeng3@55tuan.com>
2016-03-01 21:36:36 +0800

    添加首页绑定事件

--- src/com/huasheng/sysq/activity/IndexActivity.java
+++ src/com/huasheng/sysq/activity/IndexActivity.java
@@ -1,12 +1,24 @@
 package com.huasheng.sysq.activity;
 
-import com.huasheng.sysq.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.LinearLayout;
 
-public class IndexActivity extends Activity{
+import com.huasheng.sysq.R;
+
+public class IndexActivity extends Activity implements OnClickListener{
+	
+	private LinearLayout reservationLL;
+	private LinearLayout dataStaticsLL;
+	private LinearLayout systemUpdateLL;
+	private LinearLayout interviewStartLL;
+	private LinearLayout userCenterLL;
+	private LinearLayout interviewerSearchLL;
+	private LinearLayout dataUploadLL;
+	private LinearLayout helpLL;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +26,50 @@ public class IndexActivity extends Activity{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_index);
 		
+		reservationLL = (LinearLayout)findViewById(R.id.reservation_admin);
+		reservationLL.setOnClickListener(this);
 		
+		dataStaticsLL = (LinearLayout)findViewById(R.id.data_statics);
+		dataStaticsLL.setOnClickListener(this);
+		
+		systemUpdateLL = (LinearLayout)findViewById(R.id.system_update);
+		systemUpdateLL.setOnClickListener(this);
+		
+		interviewStartLL = (LinearLayout)findViewById(R.id.interview_start);
+		interviewStartLL.setOnClickListener(this);
+		
+		userCenterLL = (LinearLayout)findViewById(R.id.user_center);
+		userCenterLL.setOnClickListener(this);
+		
+		interviewerSearchLL = (LinearLayout)findViewById(R.id.interviewer_search);
+		interviewerSearchLL.setOnClickListener(this);
+		
+		dataUploadLL = (LinearLayout)findViewById(R.id.data_upload);
+		dataUploadLL.setOnClickListener(this);
+		
+		helpLL = (LinearLayout)findViewById(R.id.help);
+		helpLL.setOnClickListener(this);
+		
+	}
+
+	@Override
+	public void onClick(View view) {
+		if(view.getId() == R.id.reservation_admin){
+		}else if(view.getId() == R.id.data_statics){
+			
+		}else if(view.getId() == R.id.system_update){
+			
+		}else if(view.getId() == R.id.interview_start){
+			
+		}else if(view.getId() == R.id.user_center){
+			
+		}else if(view.getId() == R.id.interviewer_search){
+			
+		}else if(view.getId() == R.id.data_upload){
+			
+		}else if(view.getId() == R.id.help){
+			
+		}
 	}
 
 }
