169e1cb061a34f655341b54c434cd30eb2c15814
mapeng <mapeng3@yunzongnet.com>
2017-07-27 23:18:42 +0800

    版本升级：问卷升级为4，app升级为3/1.2.0

--- assets/data/version.sql
+++ assets/data/version.sql
@@ -1,1 +1,1 @@
-insert into version (id,name,publish_date,remark,is_current) values (3,'版本3','2017/7/23','2017/7/23日晚',1);
+insert into version (id,name,publish_date,remark,is_current) values (4,'版本4','2017/7/27','2017-7-27晚上',1);
